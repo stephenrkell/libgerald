d83ac49bf48cf60f73cd4cc0cc43cdb085913415
Stephen Kell <S.R.Kell@kent.ac.uk>
2019-06-12 18:10:51 +0100

    Some further cleanups, making for slightly more robust glibc build rules.

--- contrib/glibc-2.1x-debian.mk
+++ contrib/glibc-2.1x-debian.mk
@@ -1,27 +1,25 @@
-all_rtld_routines := $$(rtld-routines)
-all_rtld_routines += $$(sysdep-rtld-routines)
-all_rtld_routines += $(patsubst %.c,%.o,$(patsubst %.s,%.o,$(patsubst %.S,%.o,$(EXT_FILES))))
-
-rtld_obj_dir := $(RTLD_OBJ_DIR)
+ALL_RTLD_ROUTINES := $$(rtld-routines)
+ALL_RTLD_ROUTINES += $$(sysdep-rtld-routines)
+ALL_RTLD_ROUTINES += $(patsubst %.c,%.o,$(patsubst %.s,%.o,$(patsubst %.S,%.o,$(EXT_SRC))))
 
 # PROBLEM: when we have a fresh build of libc,
 # our rtld objects will look new, even though we
 # now have to replace them with our rebuilt one.
 # Since rebuilding the .os is cheap, we force it.
-RTLD_OS := $(shell rm -f $(rtld_obj_dir)/librtld.os \
-	rm -f $(rtld_obj_dir)/librtld.os \
-	rm -f $(rtld_obj_dir)/librtld.os.map \
-	rm -f $(rtld_obj_dir)/rtld-libc.a \
-	rm -f $(rtld_obj_dir)/rtld.os \
-	rm -f $(rtld_obj_dir)/librtld.mk \
-	rm -f $(rtld_obj_dir)/librtld.map*; \
+RTLD_OS := $(shell rm -f $(RTLD_OBJ_DIR)/librtld.os \
+	rm -f $(RTLD_OBJ_DIR)/librtld.os \
+	rm -f $(RTLD_OBJ_DIR)/librtld.os.map \
+	rm -f $(RTLD_OBJ_DIR)/rtld-libc.a \
+	rm -f $(RTLD_OBJ_DIR)/rtld.os \
+	rm -f $(RTLD_OBJ_DIR)/librtld.mk \
+	rm -f $(RTLD_OBJ_DIR)/librtld.map*; \
 	echo $(RTLD_OBJ_DIR)/librtld.os)
 
 $(RTLD_OS): $(EXT_SRC)# preload.os
-	$(MAKE) VPATH="$(shell pwd)" -C $(rtld_obj_dir) \
-	    all-rtld-routines='$(all_rtld_routines)' ;; \
+	$(MAKE) VPATH="$(shell pwd)" -C $(RTLD_OBJ_DIR) \
+	    all-rtld-routines='$(ALL_RTLD_ROUTINES)'
 
-LD_SO_VERLIST := $(rtld_obj_dir)/../ld.map
+LD_SO_VERLIST := $(RTLD_OBJ_DIR)/../ld.map
 LD_SO_DYNLIST :=
 LD_EXT_SO_OBJS := $(RTLD_OS)
 LD_SO_SONAME := ld-linux-x86-64.so.2
--- contrib/glibc-2.2x-debian.mk
+++ contrib/glibc-2.2x-debian.mk
@@ -1,34 +1,44 @@
-all_rtld_routines := $$(rtld-routines)
-all_rtld_routines += $$(sysdep-rtld-routines)
-ext_routines := $(patsubst %.c,%,$(patsubst %.s,%,$(patsubst %.S,%,$(EXT_SRC))))
-all_rtld_routines += $(ext_routines)
-rtld_obj_dir := $(RTLD_OBJ_DIR)
+ALL_RTLD_ROUTINES := $$(rtld-routines)
+ALL_RTLD_ROUTINES += $$(sysdep-rtld-routines)
+EXT_ROUTINES := $(patsubst %.c,%,$(patsubst %.s,%,$(patsubst %.S,%,$(EXT_SRC))))
+ALL_RTLD_ROUTINES += $(EXT_ROUTINES)
+
+ifeq ($(RTLD_OBJ_DIR),)
+$(error Check configuration: RTLD_OBJ_DIR should be set)
+else
+$(info RTLD_OBJ_DIR: $(RTLD_OBJ_DIR))
+endif
+ifeq ($(realpath $(RTLD_OBJ_DIR)),)
+$(error Check configuration: RTLD_OBJ_DIR does not exist: $(RTLD_OBJ_DIR))
+endif
+$(info RTLD_OBJ_DIR: $(RTLD_OBJ_DIR))
 
 # PROBLEM: when we have a fresh build of libc,
 # our rtld objects will look new, even though we
 # now have to replace them with our rebuilt one.
 # Since rebuilding the .os is cheap, we force it.
 RTLD_OS := $(shell rm -f \
-	$(rtld_obj_dir)/librtld.os \
-	$(rtld_obj_dir)/librtld.os \
-	$(rtld_obj_dir)/librtld.os.map \
-	$(rtld_obj_dir)/rtld-libc.a \
-	$(rtld_obj_dir)/rtld.os \
-	$(rtld_obj_dir)/librtld.mk \
-	$(rtld_obj_dir)/librtld.map* \
-	$(rtld_obj_dir)/librtld.map* \
-	$(foreach f,$(ext_routines),$(rtld_obj_dir)/$(f).os $(rtld_obj_dir)/$(f).os.d $(rtld_obj_dir)/$(f).test); \
+	$(RTLD_OBJ_DIR)/librtld.os \
+	$(RTLD_OBJ_DIR)/librtld.os \
+	$(RTLD_OBJ_DIR)/librtld.os.map \
+	$(RTLD_OBJ_DIR)/rtld-libc.a \
+	$(RTLD_OBJ_DIR)/rtld.os \
+	$(RTLD_OBJ_DIR)/librtld.mk \
+	$(RTLD_OBJ_DIR)/librtld.map* \
+	$(RTLD_OBJ_DIR)/librtld.map* \
+	$(foreach f,$(EXT_ROUTINES),$(RTLD_OBJ_DIR)/$(f).os $(RTLD_OBJ_DIR)/$(f).os.d $(RTLD_OBJ_DIR)/$(f).test); \
 	echo $(RTLD_OBJ_DIR)/librtld.os)
 
-$(RTLD_OS) $(rtld_obj_dir)/ld.so: do-libc-rebuild
+$(RTLD_OS) $(RTLD_OBJ_DIR)/ld.so: do-libc-rebuild
 
+# the important thing to rebuild is librtld.os
 .PHONY: do-libc-rebuild
 do-libc-rebuild: $(EXT_SRC)
-	$(MAKE) VPATH="$(shell pwd)" -C $(LIBC_SRC_DIR)/elf \
-	    objdir='$(realpath $(rtld_obj_dir)/..)' \
-	    all-rtld-routines='$(all_rtld_routines)'
+	LD_SO_LDFLAGS="$(LD_EXT_SO_LDFLAGS)" $(MAKE) VPATH="$(shell pwd)" -C $(LIBC_SRC_DIR)/elf \
+	    objdir='$(realpath $(RTLD_OBJ_DIR)/..)' \
+	    all-rtld-routines='$(ALL_RTLD_ROUTINES)' $(RTLD_OBJ_DIR)/librtld.os
 
-LD_SO_VERLIST := $(rtld_obj_dir)/../ld.map
+LD_SO_VERLIST := $(RTLD_OBJ_DIR)/../ld.map
 LD_SO_DYNLIST :=
 LD_EXT_SO_OBJS := $(RTLD_OS)
 LD_SO_SONAME := ld-linux-x86-64.so.2
