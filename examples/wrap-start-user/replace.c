d7bfbd1d11e812415d26fa3085ecdc159632219f
Stephen Kell <S.R.Kell@kent.ac.uk>
2019-06-25 18:45:56 +0100

    Remove some cruft.

--- examples/wrap-start-user/replace.c
+++
@@ -1,7 +0,0 @@
-struct link_map;
-void __new__dl_init (struct link_map *main_map, int argc, char **argv, char **env);
-void _dl_init (struct link_map *main_map, int argc, char **argv, char **env)
-{
-	__new__dl_init(main_map, argc, argv, env);
-}
-//asm ("__real__dl_init = .;");
